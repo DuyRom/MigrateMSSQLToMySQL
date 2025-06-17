8335d10f00a5e1ab5410a22f8c4ea5e72ac3264e
DuyRom <duyit192@hotmail.com>
2025-06-17 17:25:09 +0700

    refactor job_statuses

--- database/migrations/2024_09_27_100013_create_job_statuses_table.php
+++ database/migrations/2024_09_27_100013_create_job_statuses_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('job_statuses', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('start_id ')->nullable();
+            $table->bigInteger('end_id')->nullable();
             $table->string('job_id');
             $table->string('table_name');
             $table->text('stack_trace')->nullable();
             $table->boolean('completed')->default(false);
+            $table->bigInteger('records_processed')->default(0);
+            $table->float('execution_time')->default(0);
+            $table->text('error_message')->nullable();
             $table->timestamps();
         });
     }
